cf228bf359ccc2bea967c4f424a837f1a3f385fe
MariaGalecka <galecm5715195_aehit@students.vizja.pl>
2024-04-07 11:21:06 +0200

    1 commit

---
+++ Lab01.java
@@ -0,0 +1,10 @@
+public class Lab01 {
+
+    public static void main(String[] args) {
+        String imieNazwisko = "Maria Galecka";
+        int numerAlbumu = 29609;
+
+        System.out.println("Imię i nazwisko: " + imieNazwisko);
+        System.out.println("Numer albumu: " + numerAlbumu);
+    }
+}
